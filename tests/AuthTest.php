b015727e4f1adf8330b2779da46d5d41654d4149
georgina mcfadyen <gemcfadyen@hotmail.co.uk>
2016-09-15 14:30:22 +0100

    Tests added to check the correct output is given when a user hits a protected endpoint.

---
+++ tests/AuthTest.php
@@ -0,0 +1,41 @@
+<?php
+
+  use App\User;
+  use Illuminate\Foundation\Testing\WithoutMiddleware;
+  use Illuminate\Foundation\Testing\DatabaseMigrations;
+  use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+  class AuthTest extends TestCase {
+
+    public function testUnauthorisedUserIsRedirectedWhenHittingSecureEndpoint() {
+      $this->get('/api/user')->seeStatusCode(302);
+    }
+
+    public function testAuthorisedUserCanAccessProtectedEndpoint() {
+      $user = factory(App\User::class)->create();
+
+      $this->actingAs($user)
+           ->visit('/api/user')
+           ->seeStatusCode(200);
+    }
+
+    public function testDetailsOfUserAreVisibleWhenAuthenticationMiddlewareIsOff() {
+      $this->withoutMiddleware();
+      $user = factory(App\User::class)->create();
+      $user->name = 'Fred';
+      $user->email = 'fred@email.com';
+
+      $this->actingAs($user)
+            ->visit('/api/user')
+            ->seeJsonContains(['name' => 'Fred', 'email' => 'fred@email.com']);
+    }
+
+
+    public function testDetailsOfUserAreVisibleWhenUserIsAuthenticated() {
+      $user = factory(App\User::class)->create();
+      $user->name = 'Fred';
+      $user->email = 'fred@email.com';
+      $this->actingAs($user, 'api')
+      ->visit('/api/user')->seeJsonContains(['name' => 'Fred']);
+    }
+  }
