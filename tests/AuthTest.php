65a0dddb39c67e0e7740bad71f253aee57222135
georgina mcfadyen <gemcfadyen@hotmail.co.uk>
2016-09-15 15:59:23 +0100

    Removes unused imports

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -1,9 +1,6 @@
 <?php
 
   use App\User;
-  use Illuminate\Foundation\Testing\WithoutMiddleware;
-  use Illuminate\Foundation\Testing\DatabaseMigrations;
-  use Illuminate\Foundation\Testing\DatabaseTransactions;
 
   class AuthTest extends TestCase {
 
